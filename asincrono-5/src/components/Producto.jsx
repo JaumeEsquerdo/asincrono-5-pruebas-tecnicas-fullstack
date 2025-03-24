baa0e7dd51d59ae8a6f58bb14962fdfce19af8f7
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-03-24 16:08:13 +0100

    asincrono 5

--- asincrono-5/src/components/Producto.jsx
+++ asincrono-5/src/components/Producto.jsx
@@ -1,11 +1,30 @@
+import { useContext } from "react";
+
+import { ProductoContext } from "../../context/ProductoContext";
+
+
 export const Producto = () => {
+
+    const { productoActivo, activarProducto } = useContext(ProductoContext)
+
+
     return (
-    <main className='Main'>
-        <div className="Main-div">
-            <button className='Btn-details'>Product Details</button>
-        </div>
-        
-    </main>
+        <main className='Main'>
+            <div className="Main-div">
+                <div className={`Producto ${productoActivo == "silla" ? "activo" : "inactivo-arriba"}`}>
+                    <img src="/ruta" alt="Img de la silla" />
+                    <h2>Shell Dining Chair</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className={`Producto ${productoActivo == "mesa" ? "activo" : "inactivo-abajo"}`}>
+                    <img src="/ruta" alt="Img de la mesa" />
+                    <h2>Dunes Anthrazite Black</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <button className='Btn-details'>Product Details</button>
+            </div>
+
+        </main>
     );
 }
 
